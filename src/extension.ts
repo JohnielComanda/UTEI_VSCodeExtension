0942fe41d1e43751f61a72ff4fc459af83528061
Johniel Comanda <johniel.comanda@cit.edu>
2024-02-23 23:33:20 +0800

    Add new commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,96 +1,129 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
 import { gptRequestHandler } from './gptRequestHandler'
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+enum Framework {
+  // CS  harp
+  NUnit = 'NUnit',
+  XUnit = 'XUnit',
+  MSTest = 'MSTest',
+  Moq = 'Moq',
+  NSubstitute = 'NSubstitute',
+  FluentAssertions = 'FluentAssertions',
+
+  // Java
+}
+
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "utei" is now active!')
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    'utei.generateUnitTestFromHighlightedCode',
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      const code = getHighlightedCodeFromActiveEditor()
-      const output = generateUnitTestFromHighlightedCode(code)
-      console.log(output)
-      promptForOutputDestination(output)
-    },
+  // Register commands for generating unit tests for different frameworks
+  registerUnitTestCommand(context, 'utei.generateCSharpNUnit', Framework.NUnit)
+  registerUnitTestCommand(context, 'utei.generateCSharpXUnit', Framework.XUnit)
+  registerUnitTestCommand(
+    context,
+    'utei.generateCSharpMSTest',
+    Framework.MSTest,
   )
+  registerUnitTestCommand(context, 'utei.generateCSharpMoq', Framework.Moq)
+  registerUnitTestCommand(
+    context,
+    'utei.generateCSharpNSubstitute',
+    Framework.NSubstitute,
+  )
+  registerUnitTestCommand(
+    context,
+    'utei.generateCSharpFluentAssertions',
+    Framework.FluentAssertions,
+  )
+}
 
+function registerUnitTestCommand(
+  context: vscode.ExtensionContext,
+  command: string,
+  framework: Framework,
+) {
+  const disposable = vscode.commands.registerCommand(command, async () => {
+    const code = getCodeFromActiveEditor()
+    if (code) {
+      try {
+        const output = generateUnitTest(code, framework)
+        console.log(output)
+        promptForOutputDestination(output)
+      } catch (error) {
+        vscode.window.showErrorMessage('Error: ' + error)
+      }
+    }
+  })
   context.subscriptions.push(disposable)
 }
 
 async function promptForOutputDestination(output: Promise<string>) {
   const userInput = await vscode.window.showInputBox({
     prompt:
-      'Do you want to use the output in a new file (type "new") or an existing file (type "existing")?',
-    placeHolder: 'Type "new" or "existing"',
+      'Do you want to use the output in a new file (Enter the name for the new file) or Escape to copy it in your clipboard?',
+    placeHolder: 'e.g., Test.cs',
   })
 
-  if (userInput === 'new') {
-    const newFileName = await vscode.window.showInputBox({
-      prompt: 'Enter the name for the new file',
-      placeHolder: 'e.g., output.txt',
-    })
-
-    if (newFileName) {
+  if (userInput) {
+    vscode.window.showInformationMessage('Generating unit test...')
+    if (userInput != '') {
       const newFileUri = vscode.Uri.joinPath(
         vscode.workspace.workspaceFolders![0].uri,
-        newFileName,
+        userInput,
       )
+      vscode.window.showInformationMessage('Creating a new file...')
 
-      // Create the new file
       await vscode.workspace.fs.writeFile(newFileUri, Buffer.from(await output))
 
-      vscode.window.showInformationMessage(`New file "${newFileName}" created.`)
+      vscode.window.showInformationMessage(`New file "${userInput}" created.`)
     } else {
       vscode.window.showErrorMessage('Invalid file name.')
     }
-  } else if (userInput === 'existing') {
+  } else if (userInput === undefined) {
+    vscode.window.showInformationMessage('Generating unit test...')
     const editor = vscode.window.activeTextEditor
-    if (editor) {
+    if (editor && output) {
       await vscode.env.clipboard.writeText(await output)
       vscode.window.showInformationMessage(
-        'Unit test is copied in your clipboard. Kidly Paste it in your existing file',
+        'Unit test is copied in your clipboard. Kindly Paste it in your existing file',
       )
     } else {
       vscode.window.showErrorMessage(
-        'Invalid input. Please type "new" or "existing".',
+        'Invalid input. Please Type "file name" or press "Escape".',
       )
     }
   }
 }
 
-function getHighlightedCodeFromActiveEditor(): string | undefined {
+function getCodeFromActiveEditor(): string | undefined {
   const editor = vscode.window.activeTextEditor
   if (editor) {
     const selections = editor.selections
-    const highlightedCodeSnippets: string[] = []
-    selections.forEach((selection) => {
-      const highlightedCode = editor.document.getText(selection)
-      highlightedCodeSnippets.push(highlightedCode)
-    })
-
-    return highlightedCodeSnippets.join('\n')
+    console.log('selections: ', selections)
+    if (selections.length > 1) {
+      const highlightedCodeSnippets: string[] = []
+      selections.forEach((selection) => {
+        const highlightedCode = editor.document.getText(selection)
+        highlightedCodeSnippets.push(highlightedCode)
+      })
+      return highlightedCodeSnippets.join('\n')
+    }
+    console.log('whole code: ', editor.document.getText())
+    return editor.document.getText()
   }
   vscode.window.showErrorMessage('No active text editor found.')
   return undefined
 }
 
-async function generateUnitTestFromHighlightedCode(
+async function generateUnitTest(
   code: string | undefined,
+  framework: string | undefined,
 ): Promise<string> {
   const prompt = `Generate a complete usable unit test given the method:
 	\`\`\`
 	${code}
+	\`\`\`
+	using ${framework}
 	\`\`\`
 	that adheres to the proper conventions of writing unit test and return just the unit test without any addition information.`
   if (code) {
